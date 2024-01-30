0bc757ad9ecece2cb25d8af63e41c10e960dd178
Ham Ji Seong <hgs3896@naver.com>
2024-01-30 21:48:02 +0900

    Title: LED 전구 버튼, Time: 1630ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240130/LED 전구 버튼/led-bulb-button.py
+++ 240130/LED 전구 버튼/led-bulb-button.py
@@ -14,17 +14,28 @@ def get_next_state(s: int): # O(N)
     #         ns |= s & (1 << i)
     # return ns
 
-@cache
 def composition(s, rep):
-    if rep % 2 == 1:
-        s = get_next_state(s)
-        rep -= 1
-    if rep == 0:
-        return s
-    rep //= 2
-    s = composition(s, rep)
-    s = composition(s, rep)
-    return s
+    seq = [s]
+    for i in range(rep):
+        seq.append(get_next_state(seq[-1]))
+        if seq[0] == seq[-1]:
+            break
+    if seq[0] == seq[-1]:
+        rep %= (len(seq) - 1)
+        return seq[rep]
+    return seq[-1]
+
+# @cache
+# def composition(s, rep):
+#     if rep % 2 == 1:
+#         s = get_next_state(s)
+#         rep -= 1
+#     if rep == 0:
+#         return s
+#     rep //= 2
+#     s = composition(s, rep)
+#     s = composition(s, rep)
+#     return s
 
 s = composition(s, B)
 for i in range(N):
