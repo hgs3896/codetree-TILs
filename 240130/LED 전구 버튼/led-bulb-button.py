8d573ee19fc0eff314ea7a9f1d470b810db2a070
Ham Ji Seong <hgs3896@naver.com>
2024-01-30 21:33:12 +0900

    Title: LED 전구 버튼, Time: 257ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 240130/LED 전구 버튼/led-bulb-button.py
@@ -0,0 +1,30 @@
+from functools import cache
+
+N, B = map(int, input().split())
+s = sum(int(input()) << (N-1-i) for i in range(N))
+
+@cache
+def get_next_state(s: int): # O(N)
+    ns = 0
+    for i in range(N):
+        if s & (1 << (i+1)%N):
+            ns |= (1 - ((s >> i) & 1)) << i
+        else:
+            ns |= s & (1 << i)
+    return ns
+
+@cache
+def composition(s, rep):
+    if rep % 2 == 1:
+        s = get_next_state(s)
+        rep -= 1
+    if rep == 0:
+        return s
+    rep //= 2
+    s = composition(s, rep)
+    s = composition(s, rep)
+    return s
+
+s = composition(s, B)
+for i in range(N):
+    print(int(s & (1 << (N-1-i)) != 0))
