cdd99c1fdefb5a04e03d2295d1fd7c396698c2d8
Ham Ji Seong <hgs3896@naver.com>
2024-01-31 11:54:38 +0900

    Title: 백만제곱 크기의 격자, Time: 1413ms, Memory: 52MB, Status: Passed - CodeTree

--- 240131/백만제곱 크기의 격자/a-grid-that's-a-million-square.py
+++ 240131/백만제곱 크기의 격자/a-grid-that's-a-million-square.py
@@ -1,46 +1,49 @@
 from typing import Tuple, Dict, TypeAlias
 from collections import deque
+import sys
 
-N = int(input())
+input = sys.stdin.readline
 
+Coordinate: TypeAlias = Tuple[int, int]
+
+N: int = int(input())
 DR: Tuple[int, int, int, int] = (0, 1, 0,-1)
 DC: Tuple[int, int, int, int] = (1, 0,-1, 0)
-Coordinate: TypeAlias = Tuple[int, int]
 
 coords: Dict[Coordinate, bool] = {}
 for _ in range(N):
     key: Coordinate = tuple(map(int, input().split()))
-    coords[key] = -1
+    coords[key] = False
 
-visited = {}
 def process():
     dq = deque()
 
+    # Traverse groups by BFS
     groups = []
     for start in coords:
-        if coords[start] != -1:
+        if coords[start]:
             continue
-        
-        group_idx = len(groups)
         groups.append(start)
-        
+
         dq.append(start)
-        coords[start] = group_idx
+        coords[start] = True
         while dq:
             r, c = dq.popleft()
             for dr, dc in zip(DR, DC):
                 nr, nc = r+dr, c+dc
                 if (nr, nc) not in coords:
                     continue
-                if coords[(nr, nc)] != -1:
+                if coords[(nr, nc)]:
                     continue
                 groups[-1] = min(groups[-1], (nr, nc))
                 dq.append((nr, nc))
-                coords[(nr, nc)] = group_idx
+                coords[(nr, nc)] = True
     
-    count_nearby_edges = lambda r, c: sum((r+dr, c+dc) in coords for dr, dc in zip(DR, DC))
-    has_any_edges = lambda r, c: any((r+dr, c+dc) in coords for dr in range(-1, 2) for dc in range(-1, 2) if (dr, dc) != (0, 0))
+    count_nearby_groups = lambda r, c: sum((r+dr, c+dc) in coords for dr, dc in zip(DR, DC))
+    has_any_groups = lambda r, c: any((r+dr, c+dc) in coords for dr in range(-1, 2) for dc in range(-1, 2) if (dr, dc) != (0, 0))
+
     total_outer_edges = 0
+    visited = set()
     for key in groups:
         r, c = key
         for dr, dc in zip(DR, DC):
@@ -48,21 +51,21 @@ def process():
             if (nr, nc) in coords:
                 continue
             dq.append((nr, nc))
-            visited[(nr, nc)] = count_nearby_edges(nr, nc)
+            visited.add((nr, nc))
 
         group_outer_edges = 0
         while dq:
             r, c = dq.popleft()
 
-            group_outer_edges += visited[(r, c)]
+            group_outer_edges += count_nearby_groups(r, c)
             for dr, dc in zip(DR, DC):
                 nr, nc = r+dr, c+dc
                 if (nr, nc) in coords:
                     continue
                 if (nr, nc) in visited:
                     continue
-                visited[(nr, nc)] = count_nearby_edges(nr, nc)
-                if has_any_edges(nr, nc):
+                visited.add((nr, nc))
+                if has_any_groups(nr, nc):
                     dq.append((nr, nc))
 
         total_outer_edges += group_outer_edges
