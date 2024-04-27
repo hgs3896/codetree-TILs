e0b23310ab67afe919de820d23077ae82ea391e9
Ham Ji Seong <hgs3896@naver.com>
2024-04-27 21:57:13 +0900

    Title: 정수 명령어, Time: 693ms, Memory: 31MB, Status: Passed - CodeTree

--- 240427/정수 명령어/Integer-command.py
+++ 240427/정수 명령어/Integer-command.py
@@ -2,19 +2,20 @@ from sortedcontainers import SortedSet
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
-s = SortedSet()
-T = int(input())
-for _ in range(T):
-    k = int(input())
-    for _ in range(k):
+
+for _ in range(int(input())):
+    s = SortedSet()
+    
+    for _ in range(int(input())):
         args = input().split(' ')
         if args[0] == 'I':
             s.add(int(args[1]))
         elif s:
             if args[1] == '1':
-                s.remove(int(s[-1]))
+                s.remove(s[-1])
             else:
-                s.remove(int(s[0]))
+                s.remove(s[0])
+
     if s:
         print(s[-1], s[0])
     else:
