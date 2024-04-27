4a8d796c54648d5b6d0ec7240cccb9b3880c6181
Ham Ji Seong <hgs3896@naver.com>
2024-04-27 21:11:48 +0900

    Title: treeset 기본, Time: 653ms, Memory: 30MB, Status: Wrong Answer - CodeTree

---
+++ 240427/treeset 기본/treeset-basic.py
@@ -0,0 +1,34 @@
+import sys
+from sortedcontainers import SortedSet
+
+input = sys.stdin.readline
+s = SortedSet()
+
+def lb(x: int):
+    idx = s.bisect_left(x)
+    if idx < len(s):
+        return s[idx]
+    else:
+        return None
+
+def ub(x: int):
+    idx = s.bisect_right(x)
+    if idx < len(s):
+        return s[idx]
+    else:
+        return None
+
+f = {
+    'add': lambda x: s.add(x),
+    'remove': lambda x: s.remove(x),
+    'find': lambda x: print(str(x in s).lower()),
+    'lower_bound': lambda x: print(lb(x)),
+    'upper_bound': lambda x: print(ub(x)),
+    'largest': lambda: print(s[-1] if s else None),
+    'smallest': lambda: print(s[0] if s else None),
+}
+
+n = int(input())
+for _ in range(n):
+    args = input().rstrip().split(' ')
+    f[args[0]](*args[1:])
