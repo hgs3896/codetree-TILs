bfae8d438fc89de9f9462cceef8019e5c8a1d71b
Ham Ji Seong <hgs3896@naver.com>
2024-05-01 12:49:11 +0900

    Title: 점 빼기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240501/점 빼기/remove-point.py
+++ 240501/점 빼기/remove-point.py
@@ -2,11 +2,10 @@ from sortedcontainers import SortedSet
 import sys
 
 input = sys.stdin.readline
-s = SortedSet()
 
 n, m = map(int, input().split())
-for _ in range(n):
-    s.add(tuple(map(int, input().split())))
+:
+s = SortedSet(tuple(map(int, input().split())) for _ in range(n))
 for _ in range(m):
     idx = s.bisect_left((int(input()), 0))
     if idx == len(s):
