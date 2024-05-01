8485d80485826543cc0d443908027846191edab3
Ham Ji Seong <hgs3896@naver.com>
2024-05-01 12:24:19 +0900

    Title: 차이가 가장 작은 수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+arr = sorted(int(input()) for _ in range(n))
+l, r = 0, 0
+min_diff = arr[-1] - arr[0]
+while l < n:
+    while r < n and arr[r] - arr[l] < m:
+        r += 1
+    if r == n:
+        break
+    min_diff = min(min_diff, arr[r] - arr[l])
+    while l < r and arr[r] - arr[l] >= m:
+        l += 1
+print(min_diff)
